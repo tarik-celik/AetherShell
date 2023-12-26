94d3cbb1ce740f0bb81a9f57ddf07bf62a7b9eff
Tarık Çelik <141952517+tarik-celik@users.noreply.github.com>
2023-12-26 08:47:59 +0300

    Add files via upload

--- aesh.c
+++ aesh.c
@@ -77,6 +77,7 @@ while (*exit == false) {
 		scanf("%s", file);
 		FILE* File = fopen(file, "w");
 		fclose(File);
+		free(File);
 	}
 
 	if (strcmp(command, "rm") == 0) {
@@ -125,6 +126,7 @@ while (*exit == false) {
             free(sourcefile);
             free(destinationfile);
         }
+
 	if (strcmp(command, "calc") == 0) {
 		float *num1 = (float *) malloc (sizeof(float));
 		float *num2 = (float *) malloc (sizeof(float));
